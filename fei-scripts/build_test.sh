0f5ccb09a4d4f0735ef43f93fe0966f6b6c0c7b6
Feihm-npu <fei.hongming@u.nus.edu>
2025-03-30 13:54:18 +0800

    2nd 0330

--- fei-scripts/build_test.sh
+++ fei-scripts/build_test.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
-export NVIDIA_VISIBLE_DEVICES=4,5  # 确保变量对 Python 进程可见
-export CUDA_VISIBLE_DEVICES=4,5  # vLLM 通常需要 CUDA 变量
+export NVIDIA_VISIBLE_DEVICES=0  # 确保变量对 Python 进程可见
+export CUDA_VISIBLE_DEVICES=0  # vLLM 通常需要 CUDA 变量
 
-mkdir -p /home/feihm/llm-fei/Data/ATM/test_data_with_fabs
-mkdir -p /home/feihm/llm-fei/Data/ATM/test_data_with_fabs/ask_output
+epoch=1
+
+mkdir -p /home/feihm/llm-fei/Data/ATM/test_data_with_fabs1
+mkdir -p /home/feihm/llm-fei/Data/ATM/test_data_with_fabs/ask_output1
 
 echo "First part started!"
-python build_ask_gpt.py \
-    --model_name mistralai/Mixtral-8x7B-Instruct-v0.1 \
-    --world_size 2 \
+OMP_NUM_THREADS=64 PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True python /home/feihm/llm-fei/ATM-RAG/atm_train/attacker_build_data/build_ask_gpt.py \
+    --model_name /home/feihm/llm-fei/ATM-RAG/fei-scripts/unsloth_reproduction/s2_experiments/merged_for_vllm_final \
+    --world_size 1 \
     --ds_name NQ/NQ \
-    --dest_dir /home/feihm/llm-fei/Data/ATM/test_data_with_fabs/ask_output
+    --dest_dir "/home/feihm/llm-fei/Data/ATM/test_data_with_fabs/ask_output1"
+
+# OMP_NUM_THREADS=64 PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True python /home/feihm/llm-fei/ATM-RAG/fei-scripts/unsloth_reproduction/s0_data.py \
+#     --model_dir /home/feihm/llm-fei/ATM-RAG/fei-scripts/unsloth_reproduction/s2_experiments/model_1epoch_dpo \
+#     --ds_name NQ/NQ \
+#     --bz 28 \
+#     --dest_dir "/home/feihm/llm-fei/Data/ATM/test_data_with_fabs/ask_output{$epoch}"\
+#     --num_dups 5 \
+#     --bf16
 
 echo "First part completed!"
 
-python fab_merge.py \
-    --ds_name NQ/NQ \
-    --dest_dir /home/feihm/llm-fei/Data/ATM/test_data_with_fabs
+# python fab_merge.py \
+#     --ds_name NQ/NQ \
+#     --dest_dir /home/feihm/llm-fei/Data/ATM/test_data_with_fabs{$epoch}
 
-echo "Second part completed!"
+# echo "Second part completed!"
 
 
 # python generator_sft_data_prepare.py
